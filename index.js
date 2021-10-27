971d27b584fa7ef94d3dd7f52dfde331abbad05d
Victoria Gonzalez <gonzalezevic@gmail.com>
2021-10-26 23:48:35 -0700

    Complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    
+    const left = parseInt(leftNumbers, 10);
+    if (left > 0) {
+      dodger.style.left = `${left + 1}px`;
+  }
+}
